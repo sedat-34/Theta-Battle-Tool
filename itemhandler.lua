bae999283456e2e3f9e012d7e7852263e3e4ac8e
Sedat Arıtürk <94762039+MrDumbGuy@users.noreply.github.com>
2026-03-18 11:31:39 +0300

    Fixed deep copy on neoItemsSubarray and itemsSubArray
    Updated ItemHandler:trueRemove() to fix potential bugs

--- itemhandler.lua
+++ itemhandler.lua
@@ -22,12 +22,20 @@ function ItemHandler:removeItem(itemIndex) --Removes from the SubArray (and due 
     if lastItem then --The super easy last item removal
         self.itemsSubArray[#self.itemsSubArray] = nil
     else --Removes item from and rebuilds the itemssubarray array as if the removed eitem never existed.
-        local neoItemsSubarray = self.itemsSubArray
+        local neoItemsSubarray =  {}
+        
+        for k,v in self.itemsSubArray do
+            neoItemsSubarray[k] = v
+        end
+        
         for i = toRemove, #self.itemsSubArray-1 do
             neoItemsSubarray[i][1] = self.itemsSubArray[i+1][1]
         end
-        self.itemsSubArray = neoItemsSubarray
-        self.itemsSubArray[#self.itemsSubArray] = nil
+        
+        for k,v in pairs(neoItemsSubarray) do
+            self.itemsSubArray[k] = v
+        end
+
     end
 
 end
@@ -35,14 +43,23 @@ end
 function ItemHandler:trueRemove() --Removes all used items from self.items based on the logged indices.
     if #self.selected_items > 1 then
 
-        table.sort(self.selected_items)
+        local selectedItemIndices = {}
 
         for i = 1, #self.selected_items do
-            self.items[i] = nil
+            for j = 1, #self.items do
+                if self.items[j] == self.selected_items[i] then
+                    selectedItemIndices[i] = j
+                    break
+                end
+            end
         end
 
-        for i = #self.selected_items, 1, -1 do
-            if self.items[i] == nil then table.remove(self.items, i) end
+        table.sort(selectedItemIndices, function (a, b)
+            return a > b
+        end)
+
+        for i = 1, #selectedItemIndices do
+            self.items[selectedItemIndices[i]] = nil
         end
 
     end
